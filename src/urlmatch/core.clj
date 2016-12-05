a65b3d67cbd0c1c2b7af7399a3e3a58d3aa2f018
Shamash2014 <warp.buddhist@gmail.com>
2016-12-05 23:52:21 +0200

    Refactor parens and space

--- src/urlmatch/core.clj
+++ src/urlmatch/core.clj
@@ -1,7 +1,6 @@
 (ns urlmatch.core
   (require [clojure.string :as str]
-           [clojure.walk :refer :all]
-           ))
+           [clojure.walk :refer :all]))
 
 (defprotocol IRecogniseable
   (recognize [pattern url]))
@@ -16,8 +15,8 @@
 
          all-query (mapcat rest (filter #(= (first %1) "queryparam") splitted))
 
-         result (filter #(not= (first %1) "queryparam") splitted)
-       ]
+         result (filter #(not= (first %1) "queryparam") splitted)]
+
       (-> (apply hash-map (flatten result))
            (keywordize-keys)
            (assoc :queryparams all-query))))
@@ -25,56 +24,57 @@
 (defn find-binds [pattern]
   (mapcat rest (re-seq #"\?([A-z]+)\/?=?" pattern)))
 
-(defn match-query [pattern url]
+(defn match-query
+   "Match on query using pattern and query url
+   Fallback to just plain equals if any of the binds was specified"
+  [pattern url]
   (if (boolean (re-find #"\?" pattern))
     (let [
            binds (find-binds pattern)
-           escaped-pattern (str/replace pattern #"\?([A-z]+)\/?" "(.+)/?")
-         ]
+           escaped-pattern (str/replace pattern #"\?([A-z]+)\/?" "(.+)/?")]
 
         (map vector
           (flatten (map keyword binds))
           (->> (re-seq (re-pattern escaped-pattern) url)
                (mapcat rest)
                (map (partial re-find #"([\w-]+)"))
-               (mapcat rest)
-               )))
+               (mapcat rest))))
 
     (= pattern url)))
 
-(defn unite [patterns url]
+
+(defn unite
+  "Unite patterns and url
+  Will run appropriate checks, depending on patterns and url supplied"
+  [patterns url]
   (let [
-         host-match (= (:host patterns) (:host url))
-       ]
+         host-match (= (:host patterns) (:host url))]
    (when (true? host-match)
 
-     (let [
-             path-match (match-query (:path patterns) (:path url))
+     (let [path-match (match-query (:path patterns) (:path url))
 
-             binds-size (count (concat
-                                 (find-binds (:path patterns))
-                                 (for [xs (:queryparams patterns) :let [x (find-binds xs)]] x)))
+           binds-size (count (concat
+                               (find-binds (:path patterns))
+                               (for [xs (:queryparams patterns) :let [x (find-binds xs)]] x)))
 
-             query-match (apply concat
-                                (for [
-                                        query (:queryparams patterns)
-                                        :let [queried (match-query query (:query url))]
-                                     ]
-                                   queried))
+           query-match (apply concat
+                              (for [query (:queryparams patterns)
+                                    :let [queried (match-query query (:query url))]]
+                                queried))
 
-             result (apply concat (vector path-match query-match))
-          ]
-       (when (and (seq? result) (= binds-size (count result))) (vec (sort-by first result)))
-     )))
-)
+           result (apply concat (vector path-match query-match))]
+
+       (when (and (seq? result) (= binds-size (count result)))
+         (vec (sort-by first result)))))))
 
 (defrecord Pattern [matching]
   IRecogniseable (recognize [_pattern url]
-                            (let [
-                                   pattern-map (build-patterns matching)
-                                   uri (java.net.URI. url)
-                                   host (.getHost uri)
-                                   path (.getPath uri)
-                                   query (.getQuery uri)
-                                 ]
-                              (unite pattern-map (zipmap [:host :path :query] [host path query])))))
+                            (let [pattern-map (build-patterns matching)
+                                  uri (java.net.URI. url)
+                                  host (.getHost uri)
+                                  path (.getPath uri)
+                                  query (.getQuery uri)]
+
+                              (unite
+                                pattern-map
+                                (zipmap [:host :path :query] [host path query])))))
